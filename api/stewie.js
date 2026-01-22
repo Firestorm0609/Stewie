40c6d7df9b939bab374fc1a3b0f59454830d2783
BRIGHT JAMES AFOLAMI <brightafolami@gmail.com>
2026-01-22 14:27:54 +0100

    Update model name in API request

--- api/stewie.js
+++ api/stewie.js
@@ -50,7 +50,7 @@ export default async function handler(req) {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          model: 'llama3-8b-8192',
+          model: 'llama-3.1-8b-instant',
           messages,
           temperature: 0.9,
           max_tokens: 100,
