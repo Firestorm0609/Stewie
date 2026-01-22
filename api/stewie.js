b06b8fd4930b21ae8dfe304ff48a986a69fe6f55
BRIGHT JAMES AFOLAMI <brightafolami@gmail.com>
2026-01-22 14:23:44 +0100

    Change model from 'llama3-70b-8192' to 'llama3-8b-8192'

--- api/stewie.js
+++ api/stewie.js
@@ -50,7 +50,7 @@ export default async function handler(req) {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          model: 'llama3-70b-8192',
+          model: 'llama3-8b-8192',
           messages,
           temperature: 0.9,
           max_tokens: 100,
